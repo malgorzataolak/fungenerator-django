3e2caa9ac4056bb7503827537dd5577ad5eef061
Krzysztof Niedzielski <krzniedzielski@gmail.com>
2017-05-22 20:00:06 +0200

    Validation and translation fixes, restore password screen fix

--- fungeneratorapp/fungenerator/static/js/register.js
+++ fungeneratorapp/fungenerator/static/js/register.js
@@ -1,19 +1,36 @@
+$( document ).ready(function(){
+$(".helptext").html("Nick powinien zawierać jedynie liczby lub cyfry.");
+$("label[for='id_username']").html("Nick");
+$("label[for='id_first_name']").html("Imię");
+$("label[for='id_email']").html("E-mail");
+
+
+});
+
+
 $(function() {
     $("#register-form").validate({
         rules: {
-            nick: "required",
+            username: {required: true, minlength: 3},
+            first_name: {required: true, minlength: 1},
             password: {
                 required: true,
-                minlength: 6
+                minlength: 6,
+            },
+            password: {
+                required: true,
+                minlength: 6,
             },
             password2: {
                 required: true,
                 minlength: 6,
-                equalTo: "#register-password"
+                equalTo: "#id_password"
             }
         },
         messages: {
-            nick: "Podaj swój login",
+            username: "Podaj swój login (co najmniej trzy znaki)",
+            first_name: "Podaj swoje imię",
+            email: "Proszę podać prawidłowy adres email",
             password: {
                 required: "Podaj hasło",
                 minlength: "Hasło musi mieć co najmniej 6 znaków"
