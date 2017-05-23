ea6fb4518fefdc7cbc0576cf14ad6fd8cdde1314
Krzysztof Niedzielski <krzniedzielski@gmail.com>
2017-05-23 04:36:47 +0200

    Proper storage of user settings

--- fungeneratorapp/fungenerator/models.py
+++ fungeneratorapp/fungenerator/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserSettings(models.Model):
+	user = models.ForeignKey(User)
+	animals = models.BooleanField(default=False)
+	kids = models.BooleanField(default=False)
+	fails = models.BooleanField(default=False)
+	stereotypes = models.BooleanField(default=False)
+	language = models.BooleanField(default=False)
+	absurd = models.BooleanField(default=False)
+
--- fungeneratorapp/fungenerator/views.py
+++ fungeneratorapp/fungenerator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import SettingsForm
+from .models import UserSettings
 
 # Create your views here.
 
@@ -22,9 +23,24 @@ def bubblewrap(request):
 	return render(request, 'bubblewrap.html')
 
 def settings(request):
+	current_user = request.user
 	if request.method=='POST':
 		form = SettingsForm(request.POST)
 		if form.is_valid():
 			data = form.cleaned_data
+			settings_object = UserSettings.objects.get(user=current_user)
+			settings_object.animals = data['animals']
+			settings_object.kids = data['kids']
+			settings_object.fails = data['fails']
+			settings_object.stereotypes = data['stereotypes']
+			settings_object.language = data['language']
+			settings_object.absurd = data['absurd']
+			settings_object.save()
 
-	return render(request, 'settings.html')
+	try:
+		settings_object = UserSettings.objects.get(user=current_user)
+	except UserSettings.DoesNotExist:
+		settings_object = UserSettings.objects.create(user = current_user, animals=False, kids=False, fails=False, stereotypes=False, language=False, absurd=False)
+
+
+	return render(request, 'settings.html', {'settings': settings_object })
